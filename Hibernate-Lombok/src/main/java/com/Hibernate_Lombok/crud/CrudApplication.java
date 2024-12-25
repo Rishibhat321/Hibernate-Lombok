2d3e315d65047932f618d056c3e4c7e161a38bae
Rishib#321 <rishitab2209@gmail.com>
2024-12-25 16:13:07 +0530

    updated the main application.

--- Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/CrudApplication.java
+++ Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/CrudApplication.java
@@ -28,8 +28,55 @@ public class CrudApplication {
 	public CommandLineRunner commandLineRunner(EmployeeDAO employeeDAO) {
 		return runner -> {
 
-			CreateEmployee(employeeDAO);
+		//	CreateEmployee(employeeDAO);
+
+		//	CreateMultiple(employeeDAO);
+
+			readEmployee(employeeDAO);
 		};
+	}
+
+	private void readEmployee(EmployeeDAO employeeDAO) {
+
+		// create an employee object
+		System.out.println("Creating an employee object ...");
+		employee tempEmployee = new employee("Rohit", "Sharma", "rohit@gmail.com",
+				"male", 875329654);
+
+		// saving thr employee object
+		employeeDAO.createEmployee(tempEmployee);
+		System.out.println("Saved the employee object...");
+
+		// retrieving the employee object by id
+		int theId;
+        theId = tempEmployee.getEmpId();
+        System.out.println("Employee has id: " + theId);
+
+	//	System.out.println("Employee has id: " + tempEmployee.getEmpId());
+
+		employee resEmp = employeeDAO.getEmployeeId(theId);
+
+		// displaying the details of the resEmp
+		System.out.println("Details are: " + resEmp);
+
+
+	}
+
+	private void CreateMultiple(EmployeeDAO employeeDAO) {
+		// creating an employee object
+		employee tempEmp = new employee("Rishita", "Bhatnagar",
+				"rishi@gmail.com", "female", 830456748 );
+		employee tempEmp1 = new employee("Manoj", "Singh",
+				"manoj@gmail.com", "male", 985467383);
+		employee tempEmp2= new employee("Sunny", "Sharma", "sunny@gmail.com",
+				"male", 897654396);
+
+		// saving the created employee objects
+		employeeDAO.createMultipleEmployee(tempEmp);
+		employeeDAO.createMultipleEmployee(tempEmp1);
+		employeeDAO.createMultipleEmployee(tempEmp2);
+
+
 	}
 
 	private void CreateEmployee(EmployeeDAO employeeDAO) {
