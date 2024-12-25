b18e854c8ca63e52cefdba791d205131f933b7d1
Rishib#321 <rishitab2209@gmail.com>
2024-12-25 17:00:04 +0530

    Implemented CRUD Operations.

--- Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/dao/EmployeeDAOImpl.java
+++ Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/dao/EmployeeDAOImpl.java
@@ -2,6 +2,7 @@ package com.Hibernate_Lombok.crud.dao;
 
 import com.Hibernate_Lombok.crud.entity.employee;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,4 +23,34 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     public void createEmployee(employee theEmployee) {
         entityManager.persist(theEmployee);
     }
+
+    @Override
+    @Transactional
+    public void createMultipleEmployee(employee theEmployee) {
+        entityManager.persist(theEmployee);
+
+    }
+
+    @Override
+    public employee getEmployeeId(Integer theId) {
+        return entityManager.find(employee.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void updateEmployee(employee theEmployee) {
+
+        entityManager.merge(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(Integer theId) {
+
+        employee resEmployee = entityManager.find(employee.class, theId);
+
+        entityManager.remove(resEmployee);
+    }
+
+
 }
