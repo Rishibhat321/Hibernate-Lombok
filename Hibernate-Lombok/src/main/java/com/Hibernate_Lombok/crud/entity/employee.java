e0376755125677074938aae8993892a7b662785b
Rishib#321 <rishitab2209@gmail.com>
2024-12-25 16:59:25 +0530

    Created entity class. Tried lombok annotations.

--- Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/entity/employee.java
+++ Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/entity/employee.java
@@ -3,10 +3,10 @@ package com.Hibernate_Lombok.crud.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@NoArgsConstructor
+import java.math.BigInteger;
+
 @Getter
 @Setter
-@ToString
 @Entity
 @Table(name = "employee_tracker")
 public class employee {
@@ -29,15 +29,79 @@ public class employee {
     private String gender;
 
     @Column(name = "mobile")
-    private double mobile;
+    private int mobile;
 
-    public employee(String firstName, String lastName, String email, String gender, double mobile) {
+    public employee() {
+
+    }
+
+    public employee(String firstName, String lastName, String email, String gender, int mobile) {
         this.firstName= firstName;
         this.lastName=lastName;
         this.email=email;
         this.gender=gender;
         this.mobile=mobile;
 
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "employee{" +
+                "email='" + email + '\'' +
+                ", empId=" + empId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", mobile=" + mobile +
+                '}';
     }
 
 }
