6807da3ed4a0761254b9e06326a72f741bb43779
Rishib#321 <rishitab2209@gmail.com>
2024-12-25 16:59:47 +0530

    Created methods for CRUD.

--- Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/dao/EmployeeDAO.java
+++ Hibernate-Lombok/src/main/java/com/Hibernate_Lombok/crud/dao/EmployeeDAO.java
@@ -2,9 +2,19 @@ package com.Hibernate_Lombok.crud.dao;
 
 import com.Hibernate_Lombok.crud.entity.employee;
 
+import java.util.List;
+
 public interface EmployeeDAO {
 
     void createEmployee(employee theEmployee);
+
+    void createMultipleEmployee(employee theEmployee);
+
+    employee getEmployeeId(Integer theId);
+
+    void updateEmployee(employee theEmployee);
+
+    void deleteEmployee(Integer theId);
 
 
 }
